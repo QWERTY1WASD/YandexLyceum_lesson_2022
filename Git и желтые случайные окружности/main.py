6ec0d54b8c2721e78a9f6a800589d27d89b94202
il060 <il06.03.77@gmail.com>
2022-11-20 12:31:14 +0500

    Add random color for circles

--- Git и желтые случайные окружности/main.py
+++ Git и желтые случайные окружности/main.py
@@ -7,7 +7,6 @@ from PyQt5.QtWidgets import QWidget, QApplication
 
 
 class Example(QWidget):
-    CIRCLE_COLOR = QColor(255, 255, 0)
     MINIMAL_DIAMETER = 10
 
     def __init__(self):
@@ -28,9 +27,10 @@ class Example(QWidget):
         self.update()
 
     def draw_circle(self, qp):
+        color = QColor(*[random.randint(0, 255) for _ in range(3)])
         diameter = random.randint(self.MINIMAL_DIAMETER,
                                   min(self.width(), self.height()) // 2)
-        qp.setBrush(self.CIRCLE_COLOR)
+        qp.setBrush(color)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
         qp.drawEllipse(x, y, diameter, diameter)
